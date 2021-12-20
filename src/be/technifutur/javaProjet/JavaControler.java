25e233d1a41852f31f749e707a1483d26f3aa330
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2021-12-20 11:16:47 +0100

    Accesseurs dans JavaControler

--- src/be/technifutur/javaProjet/JavaControler.java
+++ src/be/technifutur/javaProjet/JavaControler.java
@@ -7,5 +7,8 @@ public class JavaControler {
     //Vérifie que les informations entrées sont justes (choix lors du menu ou nom/type d'activité encodée)
     //Gestion de l'erreur
 
+    //2 accesseurs publics pour JavaVue et JavaModel
+    public void setVue(JavaVue javaVue) {this.vue = javaVue;}
+    public void setModel(JavaModel javaModel) {this.model = javaModel;}
 
 }
--- src/be/technifutur/javaProjet/JavaVue.java
+++ src/be/technifutur/javaProjet/JavaVue.java
@@ -8,14 +8,18 @@ public class JavaVue {
     //Affichage choix secondaire
     //Affichage message d'erreur
 
+    //attributs privés pointant sur System.in
     private Scanner input = new Scanner(System.in);
+    private String error = null;
+
+    public void setError(String message){
+        this.error = message;
+    }
 
     public String saisirMenu(JavaModel model){
         String nameMenu = model.getName();
         System.out.println(nameMenu);
         System.out.println("-".repeat(nameMenu.length()));
-
-
 
         return nameMenu;
     }
