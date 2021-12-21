84afda764edd3a11b46c7b5cf3853f02f7dd7bbc
ManonKerrels <92925675+ManonKerrels@users.noreply.github.com>
2021-12-21 10:06:17 +0100

    Call (Controler)

--- src/be/technifutur/javaProjet/activités/ActivityControler.java
+++ src/be/technifutur/javaProjet/activités/ActivityControler.java
@@ -3,13 +3,9 @@ package be.technifutur.javaProjet.activités;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
 
-public class ActivityControler extends ActivityType{
+public class ActivityControler implements Callable {
     private ActivityVue vue;
     private ListActivity model;
-
-    public ActivityControler(String name, boolean registration) {
-        super(name, registration);
-    }
 
     //2 accesseurs publics pour ActivitéVue et ActivitéModel
     public void setVue(ActivityVue vue) {this.vue = vue;}
@@ -17,18 +13,19 @@ public class ActivityControler extends ActivityType{
 
 
     //méthode qui permet d'ajouter un nom d'activité et vérifie si c'est déjà présent ou pas
-    public Callable addActivityType(Callable action) {
+    public ActivityType call () {
+        boolean enregistrement = false;
         String nameActivity = vue.putActivityType();
         if(model.get(nameActivity) != null){
-            nameActivity = model.addActivityType();
+            vue.putActivityType();
+        } else {
             String inscription = vue.inscription();
-
+            if(inscription.charAt(0) == 'O'){
+                enregistrement = true;
+            }
         }
-        return action;
-    }
-
-    public Callable inscription(Callable action){
-
+        model.addActivityType(nameActivity, enregistrement);
+        return null;
     }
 
 }
